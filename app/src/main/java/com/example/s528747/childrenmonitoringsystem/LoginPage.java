bd8918d34cf1b16bb58e003c9a38397ac6e7f105
yeshwanth <yeshwanth.rangineni@gmail.com>
2017-11-19 21:17:26 -0600

    data case connections and register and login functionality

--- app/src/main/java/com/example/s528747/childrenmonitoringsystem/LoginPage.java
+++ app/src/main/java/com/example/s528747/childrenmonitoringsystem/LoginPage.java
@@ -7,14 +7,22 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class LoginPage extends AppCompatActivity {
+import com.backendless.Backendless;
+import com.backendless.BackendlessUser;
+import com.backendless.async.callback.AsyncCallback;
+import com.backendless.exceptions.BackendlessFault;
 
+public class LoginPage extends AppCompatActivity {
+    public static final String APPLICATION_ID = "E122DCE7-1E5A-FF68-FF17-5A5E6CE84200";
+    public static final String API_KEY = "AC74F97F-7106-942F-FFED-BE52CE6F7A00";
+    public static final String SERVER_URL = "https://api.backendless.com";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.loginpage);
+        Backendless.initApp(this,APPLICATION_ID,API_KEY);
 
     }
 
@@ -30,9 +38,28 @@ public class LoginPage extends AppCompatActivity {
         if(str.length()<=0||str1.length()<=0){
             Toast.makeText(getApplicationContext(),"Enter login credentials ", Toast.LENGTH_SHORT).show();
         }
-        else{ Intent in=new Intent(this,Main2Activity.class);
+        else{
+            System.out.println(str+" "+str1);
+final   Intent in=new Intent(this,Main2Activity.class);
             in.putExtra("Name",str);
-            startActivity(in);}
+            Backendless.UserService.login(str, str1, new AsyncCallback<BackendlessUser>() {
+                @Override
+                public void handleResponse(BackendlessUser response) {
+
+                    System.out.println(response);
+
+                    startActivity(in);
+
+                }
+
+                @Override
+                public void handleFault(BackendlessFault fault) {
+                    System.out.println(fault);;
+
+                }
+            });
+
+         }
     }
 
     public void signup(View V){
