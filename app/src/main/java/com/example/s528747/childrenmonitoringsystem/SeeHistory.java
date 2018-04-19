f7586d231365ea5eb5cbef012e62088f1babc008
SandeepMulakala <S528752@mail.nwmissouri.edu>
2018-04-19 17:32:07 -0500

    SeeHistoryListview

--- app/src/main/java/com/example/s528747/childrenmonitoringsystem/SeeHistory.java
+++ app/src/main/java/com/example/s528747/childrenmonitoringsystem/SeeHistory.java
@@ -1,13 +1,23 @@
 package com.example.s528747.childrenmonitoringsystem;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class SeeHistory extends AppCompatActivity {
+public class SeeHistory extends Activity
+{
+    // Array of strings...
+    ListView simpleList;
+    String countryList[] = {"04-20-2018", "04-19-2018", "04-18-2018", "04-14-2018", "03-28-2018"};
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_see_history);
+    @Override   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);      setContentView(R.layout.activity_see_history);
+//        simpleList = (ListView)findViewById(R.id.simpleListView);
+//        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
+//        simpleList.setAdapter(arrayAdapter);
+
+
     }
+
+
 }
